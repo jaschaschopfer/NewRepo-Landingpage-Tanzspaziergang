991a771de40344ef176768736a03411144ef95b9
jaschaschopfer <jascha.schopfer@stud.fhgr.ch>
2026-04-12 21:18:56 +0200

    added sponsors

--- index.php
+++ index.php
@@ -258,14 +258,51 @@
     </section>
 
     <footer>
-      <a href="privacy.html" class="privacy-link">Datenschutzerklärung</a>
-      <div class="social-media">
-        <a href="https://www.instagram.com/tanzbewegung.boll" target="_blank" rel="noopener noreferrer" aria-label="Follow us on Instagram">
-        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="currentColor">
-          <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zM12 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/>
-        </svg>
-        </a>
+      <div class="footer-top">
+        <div class="sponsors-container">
+    
+          <div class="sponsors-section">
+            <h5>Unterstützt von</h5>
+            <div class="sponsor-links">
+              <a href="https://www.fobe.sid.be.ch/de/start/lotteriefonds.html" target="_blank">Swisslos / Culture Canton de Berne</a> &nbsp;|&nbsp;
+              <a href="https://www.vechigen.ch" target="_blank">Gemeinde Vechigen</a> &nbsp;|&nbsp;
+              <a href="https://gvb.ch/de/gvb-gruppe/engagement/kulturstiftung.html" target="_blank">GVB Kulturstiftung</a> &nbsp;|&nbsp;
+              <a href="https://www.bgbern.ch" target="_blank">Burgergemeinde Bern</a> &nbsp;|&nbsp;
+              <a href="https://engagement.migros.ch/de/kulturprozent" target="_blank">Migros Kulturprozent</a> &nbsp;|&nbsp;
+              <a href="https://www.stettlen.ch" target="_blank">Gemeinde Stettlen</a> &nbsp;|&nbsp;
+              <a href="https://www.kgvechigen.ch/" target="_blank">Reformierte Kirchgemeinde Vechigen</a> &nbsp;|&nbsp;
+              <a href="https://www.ostermundigen.ch" target="_blank">Gemeinde Ostermundigen</a>
+            </div>
+          </div>
+
+          <div class="sponsors-section">
+  <h5>Herzlichen Dank an</h5>
+  <div class="sponsor-links">
+    <a href="https://www.schlossutzigen.ch/" target="_blank">Schloss Utzigen</a> &nbsp;|&nbsp;
+    <a href="https://www.printgraphic.ch" target="_blank">Print Graphic</a> &nbsp;|&nbsp;
+    <a href="https://gerber-busreisen.ch/" target="_blank">Gerber Busreisen</a> &nbsp;|&nbsp;
+    <a href="https://www.ss-vechigen.ch/" target="_blank">Sportschützen Vechigen</a> &nbsp;|&nbsp;
+    <a href="https://www.landivechigen.ch" target="_blank">Landi Vechigen</a> &nbsp;|&nbsp;
+    <a href="https://coffeebreak-laroma.ch/" target="_blank">Coffeebreak l'Aroma</a> &nbsp;|&nbsp;
+    <a href="https://www.natur-stil.ch" target="_blank">Natur-Stil</a> &nbsp;|&nbsp;
+    <a href="https://www.delinat.com/" target="_blank">Delinat</a>
+  </div>
+</div>
+
+        </div>
       </div>
+      
+      <div class="footer-bottom">
+          <a href="privacy.html" class="privacy-link">Datenschutzerklärung</a>
+        <div class="social-media">
+          <a href="https://www.instagram.com/tanzbewegung.boll" target="_blank" rel="noopener noreferrer" aria-label="Follow us on Instagram">
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="currentColor">
+            <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zM12 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/>
+          </svg>
+          </a>
+        </div>
+      </div>
+      
     </footer>
 
   </main>
